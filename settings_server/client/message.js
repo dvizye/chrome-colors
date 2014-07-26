7f9ed112ece7f40aed2ca6697ef58b3025d569fe
Erick Fuentes <efuentes@fitbit.com>
2014-07-26 16:59:04 -0700

    adding receive handler to meteor app

--- settings_server/client/message.js
+++ settings_server/client/message.js
@@ -1,18 +1,10 @@
-window.addEventListener("message", handler)
- if (parent) {
- Notifications.find().observe({
-   added: function (doc) {
-     parent.postMessage(JSON.stringify(doc), "*");
-   }
- });
-}
-console.log("runin2")
 var handler = function(e) {
   console.log("received message")
   console.log(e.data)
 }
 
-window.onmessage = function(e) {
-  alert(e.data)
-  console.log("also con")
-}
+window.addEventListener("message", handler)
+
+
+
+
