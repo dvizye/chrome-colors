870f07b6e1f943ab3795a11e110f92b794f44462
Erick Fuentes <efuentes@fitbit.com>
2014-07-26 18:14:26 -0700

    messages from extension to meteor

--- extensions/popup.js
+++ extensions/popup.js
@@ -3,15 +3,38 @@ document.addEventListener('DOMContentLoaded', function() {
     link.addEventListener('click', click);
 });
 
+var meteorWindow = document.getElementById("meteorFrame").contentWindow;
+
 function click(e) {
   // chrome.tabs.executeScript(null,
   //     {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
   // window.close();
-	var iframe = document.getElementById("meteorFrame");
-	iframe.contentWindow.postMessage("hello there!", "*");
+  	meteorWindow.postMessage({"command":"pageLoad", "args": {"url":"http://google.com"}}, "*");
 }
 
 window.onmessage = function(e) {
-	console.log("client received something")
-	console.log(e.data)
+	console.log("client received something");
+	console.log(e.data);
+}
+
+sendPageLoadMessage = function(url) {
+	var objToSend = {
+		"command": "pageLoad",
+		"args": {
+			"url":url
+		}
+	};
+	meteorWindow.postMessage(objToSend, "*");
+}
+
+sendDeltaMessage = function(url, deltas) {
+	var objToSend = {
+		"command" : "updateDelta",
+		"args" : {
+			"url" : url,
+			"deltas" : deltas
+		}
+	};
+
+	meteorWindow.postMessage(objToSend);
 }
