6705301d2e4458b211b4ebb92960d70c76adb411
Erick Fuentes <efuentes@fitbit.com>
2014-07-26 17:19:46 -0700

    fixing popup.js

--- extensions/popup.js
+++ extensions/popup.js
@@ -1,14 +1,13 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('testButton');
+    link.addEventListener('click', click);
+});
 
 function click(e) {
   // chrome.tabs.executeScript(null,
   //     {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
   // window.close();
 	var iframe = document.getElementById("meteorFrame");
-	console.log(iframe)
 	iframe.contentWindow.postMessage("hello there!", "*");
 
 }
