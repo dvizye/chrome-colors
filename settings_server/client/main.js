9ce81d57616c1eda7e67759879567dca93bada70
Matthew Tancik <tancik@mit.edu>
2014-07-26 22:18:05 -0700

    fix picker problems

--- settings_server/client/main.js
+++ settings_server/client/main.js
@@ -2,11 +2,16 @@ Template.hello.rendered=function() {
   $('#colorSelector').ColorPicker({
     color: '#0000ff',
     onShow: function (colpkr) {
-      $(colpkr).fadeIn(300);
+      $(colpkr).fadeIn(50);
+      $(colpkr).css({
+        zIndex: 10,
+        left: 22,
+        "margin-top": "9px",
+      });
       return false;
     },
     onHide: function (colpkr) {
-      $(colpkr).fadeOut(300);
+      $(colpkr).fadeOut(50);
       return false;
     },
     onChange: function (hsb, hex, rgb) {
